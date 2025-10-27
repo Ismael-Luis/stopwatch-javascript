c1ad1bda637ae9d269216d62a66eba8ffd784eed
Ismael Mesquita <ismaelluis169@gmail.com>
2025-10-27 13:24:03 -0300

    Add complete Timer exercise
    Projeto timer desenvolvido para praticar manipulação de DOM, eventos de clique e controle de intervalos com setInterval/clearInterval, praticas com JSON e localStorage.
    Inclui HTML, CSS, JS e pasta de assets.
    Nomes de variáveis e funções estão em inglês seguindo boas práticas.

---
+++ timer-javascript/assets/js/main.js
@@ -0,0 +1,58 @@
+function timer() {
+
+const timer = document.querySelector('.input-timer');
+const btnStart = document.querySelector('.btn-start');
+const btnPause = document.querySelector('.btn-pause');
+const btnReset = document.querySelector('.btn-reset');
+
+let seconds = 0
+let time;
+
+function getTimeFromSeconds(seconds) {
+  const timer = new Date(seconds * 1000);
+  return timer.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC',
+  });
+}
+
+function startTime() {
+  time = setInterval(function() {
+    seconds++
+    localStorage.setItem('tim', seconds)
+    timer.innerText = getTimeFromSeconds(seconds)
+  }, 1000)
+}
+
+document.addEventListener('click', function(e) {
+  const el = e.target
+  
+  if(el.classList.contains('btn-reset')) {
+    clearInterval(time)
+    seconds = 0
+    timer.innerText = getTimeFromSeconds(seconds)
+    timer.classList.remove('paused')
+  }
+
+  if(el.classList.contains('btn-pause')) {
+    clearInterval(time)
+    timer.classList.add('paused')
+  }
+
+  if(el.classList.contains('btn-start')) {
+    clearInterval(time)
+    startTime()
+    timer.classList.remove('paused')
+  }
+})
+
+function saveTime() {
+  const saved = localStorage.getItem('tim')
+  
+  seconds = saved ? Number(saved) : 0;
+  timer.innerText = getTimeFromSeconds(seconds)
+  timer.classList.add('paused')
+}
+saveTime()
+}
+timer()
